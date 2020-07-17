5b6436dd8e63d39973d186d50aa03e4a739c78d8
clgerhardt <lieblegerhardt@gmail.com>
2020-07-17 15:45:03 -0400

    the wip of create collection

--- models/collection.go
+++ models/collection.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/gofrs/uuid"
@@ -37,4 +38,26 @@ func GetAllCollections(conn *pgx.Conn) ([]Collection, error) {
 	}
 
 	return collections, nil
+}
+
+func (i *Collection) Create(conn *pgx.Conn, userID string) error {
+	i.Title = strings.Trim(i.Title, " ")
+	if len(i.Title) < 1 {
+		return fmt.Errorf("Title must not be empty.")
+	}
+	if i.PriceInCents < 0 {
+		i.PriceInCents = 0
+	}
+	now := time.Now()
+	fmt.Println("USER ID ", userID)
+	row := conn.QueryRow(context.Background(), "INSERT INTO item (title, notes, seller_id, price_in_cents, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, seller_id", i.Title, i.Notes, userID, i.PriceInCents, now, now)
+
+	err := row.Scan(&i.ID, &i.SellerID)
+
+	if err != nil {
+		fmt.Println(err)
+		return fmt.Errorf("There was an error creating the item")
+	}
+
+	return nil
 }
